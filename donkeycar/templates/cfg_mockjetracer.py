c929da28f213685ae296a44b4e4832023c2541e3
Brian Underhill <bunderhi@gmail.com>
2021-01-06 19:53:25 -0700

    DK setup to test camera pipeline

---
+++ donkeycar/templates/cfg_mockjetracer.py
@@ -0,0 +1,70 @@
+"""
+CAR CONFIG
+
+This file is read by your car application's manage.py script to change the car
+performance.
+
+EXAMPLE
+-----------
+import dk
+cfg = dk.load_config(config_path='~/mycar/config.py')
+print(cfg.CAMERA_RESOLUTION)
+
+"""
+
+
+import os
+
+#PATHS
+CAR_PATH = PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = os.path.join(CAR_PATH, 'data')
+PATH_MASK = '/media/C63B-4FCD/data/**/*.jpg'
+
+#VEHICLE
+DRIVE_LOOP_HZ = 20      # the vehicle loop will pause if faster than this speed.
+MAX_LOOPS = None        # the vehicle loop can abort after this many iterations, when given a positive integer.
+
+
+#9865, over rides only if needed, ie. TX2..
+PCA9685_I2C_ADDR = 0x40     #I2C address, use i2cdetect to validate this number
+PCA9685_I2C_BUSNUM = 1   #None will auto detect, which is fine on the pi. But other platforms should specify the bus num.
+
+#DRIVETRAIN
+#These options specify which chasis and motor setup you are using. Most are using SERVO_ESC.
+#DC_STEER_THROTTLE uses HBridge pwm to control one steering dc motor, and one drive wheel motor
+#DC_TWO_WHEEL uses HBridge pwm to control two drive motors, one on the left, and one on the right.
+#SERVO_HBRIDGE_PWM use ServoBlaster to output pwm control from the PiZero directly to control steering, and HBridge for a drive motor.
+
+#STEERING
+STEERING_CHANNEL = 0            #channel on the 9685 pwm board 0-15
+STEERING_LEFT_PWM = 460         #pwm value for full left steering
+STEERING_RIGHT_PWM = 290        #pwm value for full right steering
+
+#THROTTLE
+THROTTLE_CHANNEL = 1            #channel on the 9685 pwm board 0-15
+THROTTLE_FORWARD_PWM = 500      #pwm value for max forward throttle
+THROTTLE_STOPPED_PWM = 370      #pwm value for no movement
+THROTTLE_REVERSE_PWM = 220      #pwm value for max reverse throttle
+
+#Camera
+IMAGE_W = 224
+IMAGE_H = 224
+IMAGE_DEPTH = 3         # default RGB=3, make 1 for mono
+CAMERA_FRAMERATE = DRIVE_LOOP_HZ
+CAMERA_VFLIP = False
+CAMERA_HFLIP = False
+# For CSIC camera - If the camera is mounted in a rotated position, changing the below parameter will correct the output frame orientation
+CSIC_CAM_GSTREAMER_FLIP_PARM = 0 # (0 => none , 4 => Flip horizontally, 6 => Flip vertically)
+# Region of interst cropping
+# only supported in Categorical and Linear models.
+# If these crops values are too large, they will cause the stride values to become negative and the model with not be valid.
+ROI_CROP_TOP = 0                    #the number of rows of pixels to ignore on the top of the image
+ROI_CROP_BOTTOM = 0                 #the number of rows of pixels to ignore on the bottom of the image
+
+
+
+#Odometry
+HAVE_ODOM = False                   # Do you have an odometer? Uses pigpio 
+
+#Intel T265
+WHEEL_ODOM_CALIB = "calibration_odometry.json"
---
+++ donkeycar/templates/mockjetracer.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+"""
+Scripts to drive a donkey 2 car with Realsense T265 
+
+Usage:
+    manage.py (drive) [--model=<model>] [--type=(linear|categorical|rnn|imu|behavior|3d|localizer|latent)]
+    manage.py (train) [--tub=<tub1,tub2,..tubn>] [--file=<file> ...] (--model=<model>) [--transfer=<model>] [--type=(linear|categorical|rnn|imu|behavior|3d|localizer)] [--continuous] [--aug]
+
+
+Options:
+    -h --help          Show this screen.    
+    -f --file=<file>   A text file containing paths to tub files, one per line. Option may be used more than once.
+"""
+import os
+import time
+import logging
+
+from docopt import docopt
+import numpy as np
+
+import donkeycar as dk
+from donkeycar.parts.datastore import TubHandler
+from donkeycar.parts.camera import ImageListCamera
+from donkeycar.parts.controller import WebFpv
+from donkeycar.parts.actuator import PCA9685, PWMSteering, PWMThrottle
+from donkeycar.parts.realsenseT265 import RS_T265RAW
+from donkeycar.parts.nextion_controller import NextionController
+
+from donkeycar.utils import *
+
+
+def drive(cfg,verbose=True):
+    '''
+    Construct a working robotic vehicle from many parts.
+    Each part runs as a job in the Vehicle loop, calling either
+    it's run or run_threaded method depending on the constructor flag `threaded`.
+    All parts are updated one after another at the framerate given in
+    cfg.DRIVE_LOOP_HZ assuming each part finishes processing in a timely manner.
+    Parts may have named outputs and inputs. The framework handles passing named outputs
+    to parts requesting the same named input.
+    '''
+
+    
+    #Initialize car
+    V = dk.vehicle.Vehicle()
+ 
+
+    # FPS Camera image viewer
+    V.add(WebFpv(), inputs=['cam/image_array'], threaded=True)
+
+    #This part implements a system console display
+    # For now it only controls record on/off 
+    console = NextionController()
+    V.add(console, outputs=['recording'],threaded=True)  
+
+    # Mock camera feed
+    cam = ImageListCamera(path_mask=cfg.PATH_MASK)
+    V.add(cam, outputs=['cam/image_array'], threaded=True)
+
+    class ImgPreProcess():
+        '''
+        preprocess camera image for inference.
+        normalize and crop if needed.
+        '''
+        def __init__(self, cfg):
+            self.cfg = cfg
+
+        def run(self, img_arr):
+            return normalize_and_crop(img_arr, self.cfg)
+
+    inf_input = 'cam/normalized/cropped'
+    
+    V.add(ImgPreProcess(cfg),
+        inputs=['cam/image_array'],
+        outputs=[inf_input],
+        run_condition='run_pilot')
+
+    #Choose what inputs should change the car.
+    class DriveMode:
+        def run(self, mode,
+                    user_angle, user_throttle,
+                    pilot_angle, pilot_throttle):
+            if mode == 'user':
+                return user_angle, user_throttle
+
+            elif mode == 'local_angle':
+                return pilot_angle if pilot_angle else 0.0, user_throttle
+
+            else:
+                return pilot_angle if pilot_angle else 0.0, pilot_throttle * cfg.AI_THROTTLE_MULT if pilot_throttle else 0.0
+
+    V.add(DriveMode(),
+          inputs=['user/mode', 'user/angle', 'user/throttle',
+                  'pilot/angle', 'pilot/throttle'],
+          outputs=['angle', 'throttle'])
+
+    #Drive train setup
+    steering_controller = PCA9685(cfg.STEERING_CHANNEL, cfg.PCA9685_I2C_ADDR, busnum=cfg.PCA9685_I2C_BUSNUM)
+    steering = PWMSteering(controller=steering_controller,
+                                    left_pulse=cfg.STEERING_LEFT_PWM, 
+                                    right_pulse=cfg.STEERING_RIGHT_PWM)
+    
+    throttle_controller = PCA9685(cfg.THROTTLE_CHANNEL, cfg.PCA9685_I2C_ADDR, busnum=cfg.PCA9685_I2C_BUSNUM)
+    throttle = PWMThrottle(controller=throttle_controller,
+                                    max_pulse=cfg.THROTTLE_FORWARD_PWM,
+                                    zero_pulse=cfg.THROTTLE_STOPPED_PWM, 
+                                    min_pulse=cfg.THROTTLE_REVERSE_PWM)
+
+    V.add(steering, inputs=['angle'])
+    V.add(throttle, inputs=['throttle'])
+    
+    #run the vehicle
+    V.start(rate_hz=cfg.DRIVE_LOOP_HZ, 
+            max_loop_count=cfg.MAX_LOOPS)
+
+
+if __name__ == '__main__':
+
+    args = docopt(__doc__)
+    cfg = dk.load_config()
+
+    logging.basicConfig(level=30)
+    
+    if args['drive']:      
+        drive(cfg)
