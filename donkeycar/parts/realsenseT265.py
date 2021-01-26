8045c0baa0a3386a249f76ed2b0299a836de9fc8
Brian Underhill <bunderhi@gmail.com>
2021-01-26 10:34:34 -0700

    .....

--- donkeycar/parts/realsenseT265.py
+++ donkeycar/parts/realsenseT265.py
@@ -422,7 +422,7 @@ class ImgAlphaBlend(object):
         self.timer = cfg.TIMER
 
     def run(self, mask, img, camcount, infcount):
-        red = (mask*255).reshape(160,320,3)
+        red = (mask*255).reshape(1,160,320)
         redmask = np.vstack((self.fill,red)).transpose(2,0,1)
         dst = cv2.addWeighted(redmask, self.alpha, img, self.beta, 0.0)
         if (self.timer and camcount % 100 == 0 and camcount != 0):
