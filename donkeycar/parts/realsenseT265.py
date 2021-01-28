9843ab5044c4f1425332f6e73e63c50702961ca3
bunderhi <bunderhi@gmail.com>
2021-01-28 12:14:21 -0700

    add support for mutiline text on fpv

--- donkeycar/parts/realsenseT265.py
+++ donkeycar/parts/realsenseT265.py
@@ -439,3 +439,59 @@ class ImgAlphaBlend(object):
             y = label_height + baseline + 2
             dst = cv2.putText(dst,text,(2,y),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0), 2, cv2.LINE_AA) 
         return dst
+
+    def draw_text(
+        img,
+        *,
+        text,
+        uv_top_left,
+        color=(255, 255, 255),
+        fontScale=0.5,
+        thickness=1,
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        outline_color=(0, 0, 0),
+        line_spacing=1.5,
+    ):
+        """
+        Draws multiline with an outline.
+        """
+        assert isinstance(text, str)
+
+        uv_top_left = np.array(uv_top_left, dtype=float)
+        assert uv_top_left.shape == (2,)
+
+        for line in text.splitlines():
+            (w, h), _ = cv2.getTextSize(
+                text=line,
+                fontFace=fontFace,
+                fontScale=fontScale,
+                thickness=thickness,
+            )
+            uv_bottom_left_i = uv_top_left + [0, h]
+            org = tuple(uv_bottom_left_i.astype(int))
+
+            if outline_color is not None:
+                cv2.putText(
+                    img,
+                    text=line,
+                    org=org,
+                    fontFace=fontFace,
+                    fontScale=fontScale,
+                    color=outline_color,
+                    thickness=thickness * 3,
+                    lineType=cv2.LINE_AA,
+                )
+            cv2.putText(
+                img,
+                text=line,
+                org=org,
+                fontFace=fontFace,
+                fontScale=fontScale,
+                color=color,
+                thickness=thickness,
+                lineType=cv2.LINE_AA,
+            )
+
+            uv_top_left += [0, h * line_spacing]
+
+
