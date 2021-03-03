3ef1f814097308051ed84cbf95e9912bd3507cc2
bunderhi <bunderhi@gmail.com>
2021-03-02 20:13:24 -0700

    add data overlay to fpv display

--- donkeycar/parts/planner.py
+++ donkeycar/parts/planner.py
@@ -386,9 +386,12 @@ class PlanMap(object):
         acceltxt = "{:.1f}".format(accel)
         lines = deltatxt + '\n' + '\n' + acceltxt
         self.draw_text(redmask,text=lines,uv_top_left=(120,240))
+        dy = 400. - math.cos(delta) * accel
+        dx = 105. + math.sin(delta) * accel
+        cv2.arrowedLine(redmask,(105,400),(dx,dy),(0, 255, 255), 2, cv2.LINE_AA, 0, 0.1)
         ex = math.floor(105+(velturn*100.0))
         ey = math.floor(400+(velfwd*100.0))
-        cv2.arrowedLine(redmask,(105,400),(ex,ey),(0, 255, 0), 3, cv2.LINE_AA, 0, 0.1)
+        cv2.arrowedLine(redmask,(105,400),(ex,ey),(0, 255, 0), 2, cv2.LINE_AA, 0, 0.1)
         return redmask
 
  
